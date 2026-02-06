e415a7bc80621ba9b210574336983f198d251993
OUBELKACEM Ahmed <oubelkacemahmed78@gmail.com>
2026-02-06 22:51:37 +0100

    ajouter fonction qui cree la reservation et autre pour afficher les reservation et printer le ticket

--- app/Http/Controllers/BookingController.php
+++ app/Http/Controllers/BookingController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Programme;
 use App\Models\Segment; 
+use App\Models\Booking; 
 
 class BookingController extends Controller
 {
@@ -19,4 +20,49 @@ public function checkout(Request $request,$segment_id, $programme_id)
     
     return view('cart', compact('segment', 'programme','passengerCount', 'totalPrice'));
 }
+
+
+
+  public function store(Request $request)
+{
+$segmentId = $request->segment_id;
+$programmeId = $request->programme_id;
+$cardName  = $request->card_name;
+
+// $segment = Segment::findOrFail($segmentId);
+// $programme = Programme::findOrFail($programmeId);
+
+$booking = Booking::create([
+        'client_id' => auth()->id(),
+        'segment_id' => $segmentId,
+        'date_reservation' => now() ,
+        'statut' => 'Payé',
+        'siege_numero'=>1,
+        'is_completed'=>true,
+    ]);   
+    return redirect('/search');
 }
+
+
+public function index()
+{
+   
+    $reservations = Booking::where('client_id', auth()->id())
+        ->with('segment') 
+        ->latest() 
+        ->get();
+
+    return view('index', compact('reservations'));
+}
+
+} 
+
+
+
+
+
+// public function viewTicket($request){
+    
+// }
+
+
