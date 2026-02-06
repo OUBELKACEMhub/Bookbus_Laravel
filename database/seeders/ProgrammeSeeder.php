049e9eabbcf309e5eabbd02e68181e4462809aa0
OUBELKACEM Ahmed <oubelkacemahmed78@gmail.com>
2026-02-06 20:34:52 +0100

    modifier les coulun datedepart

--- database/seeders/ProgrammeSeeder.php
+++ database/seeders/ProgrammeSeeder.php
@@ -3,31 +3,41 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
-use App\Models\Route;
-use App\Models\Programme;
+use Illuminate\Support\Facades\DB;
+use Carbon\Carbon;
 
 class ProgrammeSeeder extends Seeder
 {
-
-
-public function run(): void
-{
-    $routeId = Route::first()?->trajet_id;
-
-    if ($routeId) {
-        Programme::create([
-            'jour_depart'   => 'Lundi',
-            'heure_depart'  => '08:00:00',
-            'heure_arrivee' => '12:00:00',
-            'route_id'      => $routeId,
-        ]);
+    public function run(): void
+    {
+        $routeId = DB::table('routes')->first()->id ?? 1;
         
-        Programme::create([
-            'jour_depart'   => 'Mardi',
-            'heure_depart'  => '14:30:00',
-            'heure_arrivee' => '18:30:00',
-            'route_id'      => $routeId,
-        ]);
+        
+        $startDate = Carbon::now(); 
+
+        
+        foreach (range(0, 6) as $day) {
+            $date = $startDate->copy()->addDays($day);
+
+            DB::table('programmes')->insert([
+                [
+                    'date_depart'   => $date->format('Y-m-d'),
+                    'heure_depart'  => '08:00:00',
+                    'heure_arrivee' => '12:00:00',
+                    'route_id'      => $routeId,
+                    'created_at'    => now(),
+                    'updated_at'    => now(),
+                ],
+                [
+                    'date_depart'   => $date->format('Y-m-d'),
+                    'heure_depart'  => '14:30:00',
+                    'heure_arrivee' => '18:30:00',
+                    'route_id'      => $routeId,
+                    'created_at'    => now(),
+                    'updated_at'    => now(),
+                ]
+            ]);
+        }
     }
 }
-}
+
