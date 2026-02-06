d6c71fbda8c051a2b0e93b1d5297cc1d3e82b723
OUBELKACEM Ahmed <oubelkacemahmed78@gmail.com>
2026-02-06 22:52:17 +0100

    ajouter relation avec segment

--- app/Models/Booking.php
+++ app/Models/Booking.php
@@ -3,8 +3,26 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
-
+use App\Models\Segment;
 class Booking extends Model
 {
-    
+   protected $fillable = [
+        'client_id',
+        'segment_id',
+        'date_reservation', 
+        'statut',
+        'siege_numero',
+        'is_completed',
+    ];
+
+    protected $casts = [
+        'date_reservation' => 'datetime',
+        'is_completed' => 'boolean', 
+    ];
+
+
+    public function segment()
+{
+    return $this->belongsTo(Segment::class);
+}
 }
