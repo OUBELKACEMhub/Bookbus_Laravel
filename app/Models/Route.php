0a35a0536d4965626a10ad507071d3ced7a20ada
OUBELKACEM Ahmed <oubelkacemahmed78@gmail.com>
2026-02-06 20:43:23 +0100

    cree la  relation hasMany  avec class segments et  programme

--- app/Models/Route.php
+++ app/Models/Route.php
@@ -3,8 +3,29 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Route extends Model
 {
-    //
+    protected $primaryKey = 'trajet_id';
+    
+    public $timestamps = true;
+
+    protected $fillable = [
+        'nom',
+        'description',
+        
+    ];
+
+    
+    public function segments(): HasMany
+    {
+        return $this->hasMany(Segment::class, 'trajet_id', 'trajet_id');
+    }
+
+    
+    public function programmes(): HasMany 
+    {
+        return $this->hasMany(Programme::class, 'route_id', 'trajet_id');
+    }
 }
