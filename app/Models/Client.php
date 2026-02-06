62c280fdeaa56d19b9fe32c1c5fda3d1687b7772
OUBELKACEM Ahmed <oubelkacemahmed78@gmail.com>
2026-02-06 20:41:24 +0100

    cree la  relation hasMany  avec class Booking

--- app/Models/Client.php
+++ app/Models/Client.php
@@ -4,15 +4,20 @@ namespace App\Models;
 
 class Client extends User 
 {
-    // 1. Forceh ikhdem b-table users
+  
     protected $table = 'users';
 
-    // 2. Glooq lih l-khidma (Global Scope) 
-    // Bach dima ila derti Admin::all() ijib ghir li 3ndhom role admin
+    
     protected static function booted()
     {
-        static::addGlobalScope('admin_only', function ($builder) {
+        static::addGlobalScope('client_only', function ($builder) {
             $builder->where('role', 'client');
         });
+    }
+    
+   
+    public function bookings()
+    {
+        return $this->hasMany(Booking::class, 'user_id');
     }
 }
