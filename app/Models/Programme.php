622df1f9d3f5f5184642e5b80cae76d075959b76
OUBELKACEM Ahmed <oubelkacemahmed78@gmail.com>
2026-02-06 20:42:32 +0100

    cree la  relation hasMany  avec class segments et relation belongTo avec route

--- app/Models/Programme.php
+++ app/Models/Programme.php
@@ -6,20 +6,22 @@ use Illuminate\Database\Eloquent\Model;
 class Programme extends Model
 {
     protected $fillable = ['jour_depart', 'heure_depart', 'heure_arrivee', 'route_id'];
-
-    // Relation inverse m3a Route
+    protected $casts = [
+    'date_depart' => 'datetime',
+];
+   
     public function route() {
         return $this->belongsTo(Route::class, 'route_id', 'trajet_id');
     }
 
-    // Relation m3a les Segments (1 Programme hasMany Segments)
+    
     public function segments() {
         return $this->hasMany(Segment::class);
     }
 
-    // Method f l-image
+
     public function isActive(): bool {
-        // Logic t-testi biha wach l-programme kheddam daba
         return true; 
     }
 }
+
