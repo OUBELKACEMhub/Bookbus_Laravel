c811d56ebbb56656daa4e5f2786aed85d15ae59f
OUBELKACEM Ahmed <oubelkacemahmed78@gmail.com>
2026-02-06 20:17:43 +0100

    modifier fonction search

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -3,31 +3,35 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Models\Segment; // N-importiw l'Model hna l'fouq
-use App\Models\Ville;   // Khassna l'villes bach l'form y-bqa 3amer
+use App\Models\Segment; 
+use App\Models\Ville; 
+use App\Models\Route; 
+use App\Models\Programme; 
 
 class SearchController extends Controller
 {
-    public function search(Request $request) 
-    {
-        // 1. Récupération des données men $request
-        $departure = $request->input('departure');
-        $arrival = $request->input('arrival');
-        $date = $request->input('date'); // Men l'hssan t-filteri hta b la date
+   public function search(Request $request) 
+{
+    $departure = $request->input('departure');
+    $arrival = $request->input('arrival');
+    $date = $request->input('date'); 
 
-        // 2. Recherche (Query)
-        // Mazyan n-asta3mlo "when" ila bghina l'recherche t-koun flexible
-        $trips = Segment::where('departure_city', $departure)
-                    ->where('arrival_city', $arrival)
-                    ->when($date, function ($query, $date) {
-                        return $query->whereDate('departure_time', $date);
-                    })
-                    ->get();
+    $villes = \App\Models\Ville::all();
 
-        // 3. Récupérer les villes bach l'utilisateur y-qder y-dir recherche khra
-        $villes = Ville::all();
-                    
-        // 4. Retourner la view (Smia khassha t-koun bhal l'fichier .blade.php)
-        return view('recherche', compact('trips', 'villes'));
-    }
+    
+    $programmes = \App\Models\Programme::with(['route.segments'])
+        ->whereHas('route.segments', function ($q) use ($departure, $arrival) {
+            $q->where('departure_city', $departure)
+              ->where('arrival_city', $arrival);
+        })
+        ->when($date, function ($query, $date) {
+            return $query->whereDate('date_depart', $date);
+        })
+        ->get();
+
+    return view('recherche', compact('programmes', 'villes'));
+}
+
+
+
 }
